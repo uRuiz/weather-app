e8b0c7cdc049fec9277ca575791bbaa4408ad850
Urko Ruiz <zaszur@gmailcom>
2019-02-15 10:40:40 +0100

    Code refactor

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
+const config = require("./config");
+
 const request = require("request");
 const yargs = require("yargs");
 
-const KEY = "Zq0jVLe2Bk5vMgEGoyfcm1zREem2Hz3n";
 const argv = yargs
     .options({
         a: {
             demand: true,
             alias: "address",
-            describe: "Address tp fetch weather for",
+            describe: "Address to fetch weather for",
             string: true
         }
     })
@@ -18,12 +19,24 @@ let address = encodeURIComponent(argv.address);
 
 request(
     {
-        url: `http://www.mapquestapi.com/geocoding/v1/address?key=${KEY}&location=${address}`,
+        url: `http://www.mapquestapi.com/geocoding/v1/address?key=${
+            config.KEY
+        }&location=${address}`,
         json: true
     },
     (error, response, body) => {
-        console.log(`Address: ${body.results[0].providedLocation.location}`);
-        console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
-        console.log(`Longitude: ${body.results[0].locations[0].latLng.lng}`);
+        if (error) {
+            console.log("Could not connect to the server");
+        } else if (body.status === "ZERO_RESULTS") {
+            console.log("Unable to find that address");
+        } else {
+            console.log(
+                `Address: ${body.results[0].providedLocation.location}`
+            );
+            console.log(`Latitude: ${body.results[0].locations[0].latLng.lat}`);
+            console.log(
+                `Longitude: ${body.results[0].locations[0].latLng.lng}`
+            );
+        }
     }
 );
---
+++ config.js
@@ -0,0 +1,5 @@
+const KEY = "Zq0jVLe2Bk5vMgEGoyfcm1zREem2Hz3n";
+
+module.exports = {
+    KEY
+};
